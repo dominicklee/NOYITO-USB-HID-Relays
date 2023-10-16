9393b76d5db5fcd9de46f46d5d672974fd789047
Dominick Lee <dnicky2288@yahoo.com>
2023-10-15 22:46:08 -0700

    Added console program

---
+++ Windows/RelayCmd/RelayCmd/Module1.vb
@@ -0,0 +1,59 @@
+﻿Imports System.Threading.Tasks
+Imports NoyitoRelay
+
+Module Module1
+
+    Sub Main(args As String())
+        Dim relayNumber As Integer = 0
+        Dim relayState As Integer = 0
+        Dim isAll As Boolean = False
+        Dim needsHelp As Boolean = False
+
+        If args.Length = 0 Then
+            Console.WriteLine("NOYITO USB HID Relay Command Line")
+            Console.WriteLine("For help, enter: relaycmd.exe -h")
+            Return
+        End If
+
+        ' Parsing arguments
+        For i = 0 To args.Length - 1 Step 2
+            Select Case args(i).ToLower()
+                Case "-r"
+                    If args(i + 1).ToLower() = "all" Then
+                        isAll = True
+                    Else
+                        relayNumber = Integer.Parse(args(i + 1))
+                    End If
+                Case "-s"
+                    relayState = Integer.Parse(args(i + 1))
+                Case "-h"
+                    needsHelp = True
+                Case Else
+                    Console.WriteLine("Unknown argument: " & args(i))
+                    Return
+            End Select
+        Next
+
+        If needsHelp Then
+            Console.WriteLine("NOYITO USB HID Relay Command Line" & vbNewLine)
+            Console.WriteLine("Usage: relaycmd.exe -r [relay_number|all] -s [state]")
+            Console.WriteLine("  relay_number: The number of the relay to control 1~8.")
+            Console.WriteLine("  Use 'all' to control all relays.")
+            Console.WriteLine("  state: 0 for off, 1 for on.")
+            Return
+        End If
+
+        ' Initialize NoyitoRelay device
+        Dim relayDevice As New RelayController
+        relayDevice.OpenDevice()
+
+        ' Perform Relay Operations
+        If isAll Then
+            relayDevice.SendRelays(relayState)
+        Else
+            relayDevice.SendRelay(relayNumber, relayState)
+        End If
+
+        relayDevice.CloseDevice()
+    End Sub
+End Module
---
+++ Windows/RelayCmd/RelayCmd/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("RelayCmd")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("GyroPalm, LLC")>
+<Assembly: AssemblyProduct("RelayCmd")>
+<Assembly: AssemblyCopyright("Copyright © 2023 by GyroPalm, LLC.")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("1f152e45-21e9-4415-8bd2-264370a71df7")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
